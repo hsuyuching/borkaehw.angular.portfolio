149308424e7818abb34b1643f2a5f2cf005e5e55
borkaehw <borkaehw@umich.edu>
2019-04-14 18:45:05 -0600

    Work experience

--- borkaehw-portfolio/src/app/app.module.ts
+++ borkaehw-portfolio/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
